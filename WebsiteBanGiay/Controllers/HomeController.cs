686a5e03ff5ecb28d6e045e2cf87210441d2950e
huybin1205 <trangiahuysn1998@gmail.com>
2019-03-19 18:03:49 +0700

    first commit

---
+++ WebsiteBanGiay/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanGiay.Models;
+
+namespace WebsiteBanGiay.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseDataContext db = new DatabaseDataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult SliderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult BoxesPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult FeaturesParital()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ProductsPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavigationPartial()
+        {
+            var list = from s in db.Loais select s;
+            return PartialView(list);
+        }
+
+        public PartialViewResult BranchPartial()
+        {
+            var list = from s in db.CuaHangs select s;
+            return PartialView(list);
+        }
+    }
+}
---
+++ WebsiteBanGiay/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanGiay.Models;
+
+namespace WebsiteBanGiay.Controllers
+{
+    public class UserController : Controller
+    {
+        DatabaseDataContext db = new DatabaseDataContext();
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(KhachHang kh)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Thêm khách hàng
+                    db.KhachHangs.InsertOnSubmit(kh);
+                    //Lưu lên csdl
+                    db.SubmitChanges();
+                    RedirectToAction("Login");
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Loi = ex.ToString();
+            }
+            return View();
+        }
+    }
+}
